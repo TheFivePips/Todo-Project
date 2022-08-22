7752573431447af071129564f7c82dd4e19d573e
sam gardner <samgardner20@gmail.com>
2022-08-22 15:21:10 -0400

    got the core functionailty to work with this new set up

--- src/createNewTodo.js
+++ src/createNewTodo.js
@@ -1,11 +1,12 @@
 // import { format } from 'date-fns'
 
 export default class Todo {
-    constructor(title, date, priority, project) {
+    constructor(title, date, priority, projectId, id) {
         this.title = title
         this.date = date
         this.priority = priority
-        this.project = project
+        this.projectId = projectId
+        this.id = id
     }
     getTodoTitle() {
         return this.title
@@ -25,5 +26,45 @@ export default class Todo {
     setTodoPriority(priority){
         this.priority = priority
     }
+    getTodoHTML(){
+
+        const todoElement = document.createElement('div')
+        todoElement.classList.add('todo')
+
+        const checkBox = document.createElement('input')
+        checkBox.setAttribute('type', 'checkbox')
+
+        const todoTitle = document.createElement('p')
+        todoTitle.classList.add('todoTitle')
+        todoTitle.textContent= this.title
+
+        const todoDate = document.createElement('p')
+        todoDate.textContent = this.date
+        
+        const todoPriority =document.createElement('p')
+        todoPriority.textContent = this.priority
+
+        const editTodoBtn = document.createElement('button')
+        editTodoBtn.setAttribute('type', 'submit')
+        editTodoBtn.classList.add('editTodoBtn')
+        editTodoBtn.textContent = "Edit"
+    //    editTodoBtn.addEventListener
+
+        const deleteTodoBtn = document.createElement('button')
+        deleteTodoBtn.classList.add('deleteTodoBtn')
+        deleteTodoBtn.setAttribute('type', 'submit')
+        deleteTodoBtn.textContent = "Delete"
+        // deleteTodoBtn.addEventListener
+
+        todoElement.appendChild(checkBox)
+        todoElement.appendChild(todoTitle)
+        todoElement.appendChild(todoDate)
+        todoElement.appendChild(todoPriority)
+        todoElement.appendChild(editTodoBtn)
+        todoElement.appendChild(deleteTodoBtn)
+
+        return todoElement
+    }
+
     
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
 import './style.css'
 import Logo from "./assets/StrykThru.png"
+import Todo from './createNewTodo'
+import {v4 as uuidv4} from 'uuid';
+
+
 const logo = document.querySelector('.logo')
 logo.src = Logo
+
+const projects = []
+
+const main = document.querySelector('.main')
+
+const addTodoBtn = document.querySelector('.addTodoBtn')
+addTodoBtn.addEventListener('click', function(event){
+    let todoTitle = document.querySelector('.todoTxtInput').value
+    let todoDate = document.querySelector('.addTodoDate').value
+    let todoPriority = document.querySelector('.addTodoPriority').value
+    let defaultID = document.querySelector('.defaultProject').id
+    
+    
+    // right now just do default project id
+    event.preventDefault()
+    const newTodo = new Todo(todoTitle, todoDate, todoPriority, defaultID, uuidv4())
+    main.appendChild(newTodo.getTodoHTML())
+})
